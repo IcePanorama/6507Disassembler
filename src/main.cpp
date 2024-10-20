0f4015811bd7c96a34d559fd70a486b851ff6793
IcePanorama <thriftedroot@protonmail.com>
2024-10-19 20:47:28 -0400

    Loading binary file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
+#include <format>
+#include <fstream>
 #include <iostream>
+#include <stdexcept>
+#include <string>
 
 int
 main (void)
 {
-  std::cout << "Hello world!\n";
+  const std::string filename = "ET.a26";
+  std::ifstream file (filename, std::ios::binary);
+  if (!file)
+    throw std::runtime_error (
+        std::format ("Error opening file, {}\n.", filename));
+
   return 0;
 }
