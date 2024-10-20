7ef4c630d0aea57c0e484173aaf90968e19262ad
IcePanorama <thriftedroot@protonmail.com>
2024-10-19 20:30:30 -0400

    Initial commit.

---
+++ build/CMakeFiles/6507Disassembler.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/main/Dev/C++/6507Disassembler/6507Disassembler"
+  "/home/main/Dev/C++/6507Disassembler/6507Disassembler.pdb"
+  "CMakeFiles/6507Disassembler.dir/src/main.cpp.o"
+  "CMakeFiles/6507Disassembler.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/6507Disassembler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
