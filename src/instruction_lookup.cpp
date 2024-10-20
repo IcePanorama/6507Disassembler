0757d85875bd3e1e8ecc5d0f0d8076febc5926f5
IcePanorama <thriftedroot@protonmail.com>
2024-10-19 22:21:07 -0400

    Fixed typo.

--- src/instruction_lookup.cpp
+++ src/instruction_lookup.cpp
@@ -2,7 +2,7 @@
 #include "addressing_mode.hpp"
 #include "instruction.hpp"
 
-const std::unordered_map<std::uint8_t, Instruction> &
+const std::unordered_map<uint8_t, Instruction> &
 InstructionLookupTable::get_table (void)
 {
   /** @see: http://www.6502.org/tutorials/6502opcodes.html */
