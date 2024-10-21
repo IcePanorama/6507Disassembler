86d3e12b523236fab9e9bf9dc5149cc71e687a0f
IcePanorama <thriftedroot@protonmail.com>
2024-10-20 23:29:46 -0400

    Fixed typo in 0x4C instruction.

--- src/instruction_lookup.cpp
+++ src/instruction_lookup.cpp
@@ -9,7 +9,7 @@ InstructionLookupTable::get_table (void)
   /** @see: http://www.6502.org/tutorials/6502opcodes.html */
   static const std::unordered_map<uint8_t, Instruction> table = {
     { 0xAD, Instruction ("LDA $", 0xAD, AM_ABSOLUTE, 2, 4) },
-    { 0x4C, Instruction ("JMP $", 0x4C, AM_ABSOLUTE, 2, 4) }
+    { 0x4C, Instruction ("JMP $", 0x4C, AM_ABSOLUTE, 2, 3) }
   };
   return table;
 }
