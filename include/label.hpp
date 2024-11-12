9188d0a60cb173d70a7c7d9e915c1f78e2d0e5d5
IcePanorama <thriftedroot@protonmail.com>
2024-11-12 16:11:58 -0500

    Label to_string method only returns the label name iff num_usages > 1.

--- include/label.hpp
+++ include/label.hpp
@@ -7,15 +7,18 @@
 
 class Label
 {
-  static std::unordered_map<uint16_t, const Label> label_lookup;
+  static std::unordered_map<uint16_t, Label> label_lookup;
 
   uint16_t address_;
-  std::string label;
+  std::string name;
 
   Label (uint16_t address);
 
 public:
-  static const Label &get_label (uint16_t address);
+  static Label &get_label (uint16_t address);
+
+  uint32_t num_usages;
+
   std::string to_string (void) const;
 };
 
--- src/label.cpp
+++ src/label.cpp
@@ -2,18 +2,21 @@
 
 #include <format>
 
-std::unordered_map<uint16_t, const Label> Label::label_lookup;
+std::unordered_map<uint16_t, Label> Label::label_lookup;
 
-Label::Label (uint16_t address) : address_ (address)
+Label::Label (uint16_t address)
+    : address_ (address),
+      name (std::format ("LABEL_{:04d}", label_lookup.size ())), num_usages (1)
 {
-  this->label = std::format ("LABEL_{:04d}", label_lookup.size ());
 }
 
-const Label &
+Label &
 Label::get_label (uint16_t address)
 {
   if (!label_lookup.contains (address))
     label_lookup.insert ({ address, Label (address) });
+  else
+    label_lookup.at (address).num_usages++;
 
   return label_lookup.at (address);
 }
@@ -21,5 +24,6 @@ Label::get_label (uint16_t address)
 std::string
 Label::to_string (void) const
 {
-  return this->label;
+  return this->num_usages > 1 ? this->name
+                              : std::format ("{:04X}", this->address_);
 }
