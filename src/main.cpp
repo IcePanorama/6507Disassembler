1cea58f20d5b7b9718bc286c1cb259a679422f1b
IcePanorama <thriftedroot@protonmail.com>
2024-11-19 18:15:50 -0500

    Taking output file as command line argument.

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,22 @@
 int
 main (int argc, char **argv)
 {
-  if (argc == 1)
+  if (argc < 3)
     {
-      std::cerr << std::format ("Invalid usage. Try {} path/to/binary/file\n",
-                                argv[0]);
+      std::cerr << std::format (
+          "Invalid usage. Try {} path/to/binary/file path/to/output/file\n",
+          argv[0]);
       return -1;
     }
 
   const std::string input_filename = argv[1];
-  const std::string output_filename = "output.asm";
-  Disassembler disassembler;
+  std::string output_filename = argv[2];
 
+  /* add file extension, if not given one. */
+  if (output_filename.find (".") == std::string::npos)
+    output_filename.append (".asm");
+
+  Disassembler disassembler;
   try
     {
       disassembler.initialize (input_filename, output_filename);
