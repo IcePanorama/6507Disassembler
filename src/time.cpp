785c1fd7eb69f360c349d3a24fac7fa96593f941
IcePanorama <thriftedroot@protonmail.com>
2024-11-20 14:23:11 -0500

    Refactored code.

--- src/time.cpp
+++ src/time.cpp
@@ -22,11 +22,12 @@
 std::string
 get_current_time (void)
 {
+  /* There might be a better way to do this, but this works. */
   auto now = std::chrono::system_clock::now ();
   std::time_t now_c = std::chrono::system_clock::to_time_t (now);
   std::tm *now_tm = std::localtime (&now_c);
 
-  return std::format ("{:d}{:d}{:d} @ {:02d}:{:02d}:{:02d}",
+  return std::format ("{:d}-{:d}-{:d} @ {:02d}:{:02d}:{:02d}",
                       now_tm->tm_year + 1900, now_tm->tm_mon + 1,
                       now_tm->tm_mday, now_tm->tm_hour, now_tm->tm_min,
                       now_tm->tm_sec);
